0954aa5d8a5362e5ad8eebe0fae8042e77924f44
James <112836642+JamesB0000@users.noreply.github.com>
2022-09-13 12:31:42 +0900

    Add files via upload

--- js/slick_option.js
+++ js/slick_option.js
@@ -3,6 +3,8 @@ $(function(){
         lazyLoad: 'ondemand', // ondemand progressive anticipated
         infinite: true,
         arrows:false,
+        autoplay:true,
+        autoplayspeed:5000,
         dots:true
       });
 })
